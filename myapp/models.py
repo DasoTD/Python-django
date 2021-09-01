132c1d3c53766c8104a6e118914b0ac641f5cc50
david.daso <david.daso@sterling.ng>
2021-08-31 20:55:47 -0700

    first commit

---
+++ myapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Feature(models.Model):
+    name = models.CharField(max_length=100)
+    details =  models.CharField(max_length=1000)
+
+    def __str__(self) :
+        return self.name 
